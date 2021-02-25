ddacf4fca578b7114e7c64b05f4b7ec34453b63f
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2021-02-25 12:22:38 -0500

    UP

--- create_dist.sh
+++ create_dist.sh
@@ -1,9 +1,9 @@
 TOOLS_DIR=tools.new
 SRSLTE_DIR=$PWD
 INST_DIR=install.sd.linux
+echo 'export LD_LIBRARY_PATH=$PWD/lib:.' > ${INST_DIR}/setup.source
 cp -rf $SRSLTE_DIR/$TOOLS_DIR/boost_for_android/build/install/libs/arm64-v8a/* ${SRSLTE_DIR}/${INST_DIR}/lib/.
-for i in fftw-3.3.8 libconfig mbedtls libzmq lksctp-tools; do
+for i in fftw-3.3.8 libconfig mbedtls libzmq lksctp-tools iris; do
 cp -rf $SRSLTE_DIR/$TOOLS_DIR/$i/install/lib/* ${SRSLTE_DIR}/${INST_DIR}/lib/. ;
 done
 
-echo 'export LD_LIBRARY_PATH=$PWD/lib:.' > ${INST_DIR}/setup.source
