120648183f93ccb2bf919dc77572b579f55989a7
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2021-04-08 11:06:50 -0400

    Added share directory of uhd to dist

--- create_dist.sh
+++ create_dist.sh
@@ -10,12 +10,14 @@ echo 'export LD_LIBRARY_PATH=$PWD/lib64:$PWD/lib:.' >> ${INST_DIR}/setup.source
 if [ -d $SRSLTE_DIR/$TOOLS_DIR/boost_for_android/build/install/libs/arm64-v8a ]; then
     cp -rf $SRSLTE_DIR/$TOOLS_DIR/boost_for_android/build/install/libs/arm64-v8a/* ${SRSLTE_DIR}/${INST_DIR}/lib/.
 fi
+mkdir -p $INST_DIR/share
 mkdir -p $INST_DIR/lib 
 mkdir -p $INST_DIR/lib64 
 for i in boost_1_69_0 fftw-3.3.8 libconfig mbedtls libzmq lksctp-tools iris libusb uhd; do
 install_comp_dir=$SRSLTE_DIR/$TOOLS_DIR/$i/install
 if [ "$i" = "uhd" ]; then
 install_comp_dir=$SRSLTE_DIR/$TOOLS_DIR/$i/host/install
+cp -rf $install_comp_dir/share/* ${SRSLTE_DIR}/${INST_DIR}/share/. ;
 fi
 if [ -d $install_comp_dir/lib ]; then
     cp -rf $install_comp_dir/lib/* ${SRSLTE_DIR}/${INST_DIR}/lib/. ;
