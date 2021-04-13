3d2d30f5c77aded4cd99626447b144f93b6f08eb
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2021-04-13 08:16:22 -0400

    Added bin

--- create_dist.sh
+++ create_dist.sh
@@ -10,6 +10,7 @@ echo 'export LD_LIBRARY_PATH=$PWD/lib64:$PWD/lib:.' >> ${INST_DIR}/setup.source
 if [ -d $SRSLTE_DIR/$TOOLS_DIR/boost_for_android/build/install/libs/arm64-v8a ]; then
     cp -rf $SRSLTE_DIR/$TOOLS_DIR/boost_for_android/build/install/libs/arm64-v8a/* ${SRSLTE_DIR}/${INST_DIR}/lib/.
 fi
+mkdir -p $INST_DIR/bin
 mkdir -p $INST_DIR/share
 mkdir -p $INST_DIR/lib 
 mkdir -p $INST_DIR/lib64 
@@ -18,6 +19,9 @@ install_comp_dir=$SRSLTE_DIR/$TOOLS_DIR/$i/install
 if [ "$i" = "uhd" ]; then
 install_comp_dir=$SRSLTE_DIR/$TOOLS_DIR/$i/host/install
 cp -rf $install_comp_dir/share/* ${SRSLTE_DIR}/${INST_DIR}/share/. ;
+fi
+if [ -d $install_comp_dir/bin ]; then
+    cp -rf $install_comp_dir/bin/* ${SRSLTE_DIR}/${INST_DIR}/bin/. ;
 fi
 if [ -d $install_comp_dir/lib ]; then
     cp -rf $install_comp_dir/lib/* ${SRSLTE_DIR}/${INST_DIR}/lib/. ;
